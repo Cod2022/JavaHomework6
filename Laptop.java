937fd3e64095506ffe607b268b6d8fd30bfa77e0
My Name <Me@gmail.com>
2022-12-27 13:31:19 +0400

    task started

---
+++ Laptop.java
@@ -0,0 +1,89 @@
+/*
+ *  Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+    Создать множество ноутбуков.
+    Написать метод, который будет запрашивать у пользователя критерий (или критерии) фильтрации и выведет ноутбуки, отвечающие фильтру. Критерии фильтрации можно хранить в Map. Например:
+    “Введите цифру, соответствующую необходимому критерию:
+    1 - ОЗУ
+    2 - Объем ЖД
+    3 - Операционная система
+    4 - Цвет …
+    Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
+    Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям    
+ */
+
+public class Laptop {
+    private int ram;
+    private int harDriveCapacity; 
+    private String operatingSystem;
+    private String color;
+    private int screenSize;
+    private boolean sold;
+
+    public Laptop(int ram, int harDriveCapacity, String operatingSystem, String color, int screenSize){
+        this.ram = ram;
+        this.harDriveCapacity = harDriveCapacity;
+        this.operatingSystem = operatingSystem;
+        this.color = color;
+        this.screenSize = screenSize;
+    }
+
+    public Laptop() {
+
+    }
+
+    public int getRam () {
+        return ram;
+    }
+    
+
+    public void setRam (int ram) {
+        this.ram = ram;
+    }
+
+    public int gethardDrive () {
+        return harDriveCapacity;
+    }
+    
+
+    public void sethardDrive (int harDriveCapacity) {
+        this.harDriveCapacity = harDriveCapacity;
+    }
+
+    public String getOperatingSystem () {
+        return operatingSystem;
+    }
+    
+
+    public void setOperatingSystem (String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
+    public String getColor () {
+        return color;
+    }
+    
+
+    public void setColor (String color) {
+        this.color = color;
+    }
+
+    public int getScreenSize () {
+        return screenSize;
+    }
+    
+
+    public void setScreenSize (int screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("RAM: %d Hard drive: %d Operating system: %s Color: %s Screen size: %d;", ram, harDriveCapacity, operatingSystem, color, screenSize);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        var t = (Laptop) o;
+        return ram == t.ram; 
+    }
+}
---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Laptop laptop1 = new Laptop(2, 250, "Windows", "Чёрный", 15);
+        Laptop laptop2 = new Laptop(2, 320, "Windows", "Чёрный", 15);
+        Laptop laptop3 = new Laptop(4, 320, "MacOs", "Серый", 15);
+        Laptop laptop4 = new Laptop(4, 500, "MacOs", "Серый", 15);
+        Laptop laptop5 = new Laptop(4, 500, "Windows", "Серый", 17);
+        Laptop laptop6 = new Laptop(8, 500, "Windows", "Чёрный", 17);
+        Laptop laptop7 = new Laptop(8, 1000, "MacOs", "Чёрный", 17);
+        Laptop laptop8 = new Laptop(16, 1000, "Astra Linux", "Белый", 17);
+        Laptop laptop9 = new Laptop(16, 1000, "RedOS", "Белый", 15);
+        Laptop laptop10 = new Laptop(16, 1500, "Linux", "Чёрный", 17);
+
+        Laptop ram = new Laptop();
+        ram.setRam(4);
+
+        HashSet<Laptop> laptopsSet = new HashSet<>();
+        laptopsSet.add(laptop1);
+        laptopsSet.add(laptop2);
+        laptopsSet.add(laptop3);
+        laptopsSet.add(laptop4);
+        laptopsSet.add(laptop5);
+        laptopsSet.add(laptop6);
+        laptopsSet.add(laptop7);
+        laptopsSet.add(laptop8);
+        laptopsSet.add(laptop9);
+        laptopsSet.add(laptop10);
+
+        System.out.println(laptopsSet);
+        System.out.println(laptop3.equals(ram));
+        
+
+        
+    }
+}
